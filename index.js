9972875928126f2483fa3445c0d32787eef20cdb
pearllitchfield <ptlitchfield@icloud.com>
2025-04-13 13:12:21 +0200

    Updated index.js
    Added functions to index.js file.

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+function refreshWeather(response) {
+  console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "7da7782c88c36f93atbb1b04a1aebo6b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
